3f2cc05683075d988fedd3ba91169b75778992e5
amiceli <miceli.antoine@gmail.com>
2013-11-23 17:01:34 +0100

    editor

--- config/adapters.js
+++ config/adapters.js
@@ -35,10 +35,10 @@ module.exports.adapters = {
 
     module: 'sails-mysql',
     host: 'localhost',
-    user: 'kh',
+    user: 'root',
     // Psst.. You can put your password in config/local.js instead
     // so you don't inadvertently push it up if you're using version control
-    password: 'BTGnhUrKx3CE7EJh', 
+    password: 'miceli', 
     database: 'kh'
   }
 };
